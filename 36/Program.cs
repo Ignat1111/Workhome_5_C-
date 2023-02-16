36e45926c1e29f6543b9b50db485ab765d206040
Ignat lazinkov <lazinkovignat@gmail.com>
2023-02-16 16:56:59 +0300

    добавил пропуски

--- 36/Program.cs
+++ 36/Program.cs
@@ -2,7 +2,10 @@
 // случайными числами. Найдите сумму элементов, стоящих на
 // нечётных позициях.
 // [3, 7, 23, 12] -> 19
-// [-4, -6, 89, 6] -> 0
+// [-4, -6, 89, 6] -> 0 
+
+
+
 
 Console.WriteLine("Введите размер массива  ");
 int size = Convert.ToInt32(Console.ReadLine());
@@ -34,3 +37,6 @@ void PrintArray(int[] numbers)
    Console.Write("]");
    Console.WriteLine();
 }
+
+
+
